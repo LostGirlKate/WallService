1af9b699a79ff58b244a11a8269094270f0c3afc
Lost Girl <ekaterina_kryukova90@mail.ru>
2024-03-09 19:36:05 +0300

    Test commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,7 @@ fun main() {
     println(wallService.add(Post(133, 133, 133, "Запись 1")))
     wallService.add(Post(133, 133, 133, "Запись 2"))
     wallService.add(Post(133, 133, 133, "Запись 3"))
-    val update = Post(133, 133, 133, "Запись 1")
+    val update = Post(133, 133, 133, "Запись 0")
     update.id = 2
     println(wallService.update(update))
 }
